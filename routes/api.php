40072728788108b7369ad7f7c5232841f5341da7
batranmhtali <batranmhtali@gmail.com>
2026-07-03 17:23:32 +0200

    security: supprimer route run-migrate

--- routes/api.php
+++ routes/api.php
@@ -87,7 +87,3 @@ Route::prefix('admin')->middleware('auth:api')->group(function () {
 });
 
 
-Route::get('/run-migrate', function() {
-    \Illuminate\Support\Facades\Artisan::call('migrate', ['--force' => true]);
-    return response()->json(['output' => \Illuminate\Support\Facades\Artisan::output()]);
-});
